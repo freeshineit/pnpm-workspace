9b4dc13f0b98cf1d3d95dbcacb5cd8aed973183a
sankong <xiaoshaoqq@gmail.com>
2023-07-06 01:10:32 +0800

    ci: webpack-cli

--- packages/pkg1/webpack.config.js
+++ packages/pkg1/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path")
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: "src/index.js",
-    out: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js"
-    }
-}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+};
