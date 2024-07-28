b7d981d6d37a37899ec053fa869b012ac8a28307
ShineShao <xiaoshaoqq@gmail.com>
2024-07-29 01:44:14 +0800

    feat: site alias

--- .prettierrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ...require('eslint-config-xx/prettier.config'),
-  printWidth: 120,
-};
--- site/src/stories/Button.stories.ts
+++ site/src/stories/Button.stories.ts
@@ -1,50 +1,35 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
-import { fn } from '@storybook/test';
-import type { ButtonProps } from './Button';
-import { Button } from './Button';
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { fn } from "@storybook/test";
+import type { ButtonProps } from "./Button";
+// import { Button } from './Button';
+import add from "@common/utils";
+
+type Args = [number, number];
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Button',
-  tags: ['autodocs'],
-  render: (args) => Button(args),
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
-  },
-  args: { onClick: fn() },
-} satisfies Meta<ButtonProps>;
+  title: "Example/Button",
+  tags: ["autodocs"],
+  render: (args: Args) => `<span>${add(args[0], args[1])}<span>`,
+  argTypes: [{ control: "number" }, { control: "number" }],
+} satisfies Meta<Args>;
 
 export default meta;
-type Story = StoryObj<ButtonProps>;
+
+type Story = StoryObj<Args>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
+  args: [1, 2],
 };
 
 export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
+  args: [2, 2],
 };
 
 export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
+  args: [9, 2],
 };
-
 export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+  args: [5, 2],
 };
