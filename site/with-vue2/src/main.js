e55ab41b2ad04a563083f537b56a533ac8155833
ShineShao <xiaoshaoqq@gmail.com>
2024-10-02 17:50:08 +0800

    ci: eslint ignore

--- site/with-vue2/src/main.js
+++ site/with-vue2/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 new Vue({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   render: (h) => h(App),
 }).$mount("#app");
