cfa45c4d920e68364a199d5ff279cdfbcaf3e54b
ShineShao <xiaoshaoqq@gmail.com>
2024-10-02 17:57:53 +0800

    ci: umd exports

--- packages/rollup/src/index.mjs
+++ packages/rollup/src/index.mjs
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-dynamic-delete */
 /* eslint-disable @typescript-eslint/dot-notation */
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import swc from '@rollup/plugin-swc';
-import serve from 'rollup-plugin-serve';
-import { upperCamel } from '@skax/camel';
-import { dts } from 'rollup-plugin-dts';
-import copy from 'rollup-plugin-copy';
-import eslint from '@rollup/plugin-eslint';
-import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
-import dayjs from 'dayjs';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import swc from "@rollup/plugin-swc";
+import serve from "rollup-plugin-serve";
+import { upperCamel } from "@skax/camel";
+import { dts } from "rollup-plugin-dts";
+import copy from "rollup-plugin-copy";
+import eslint from "@rollup/plugin-eslint";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import dayjs from "dayjs";
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 /**
  * @description rollup config function
@@ -35,7 +35,7 @@ function generateConfig(pkg, configs) {
 * Released under the MIT License.
 */`;
 
-  const input = pkg.main || 'src/index.ts';
+  const input = pkg.main || "src/index.ts";
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const externals = Object.keys(pkg?.devDependencies || {});
 
@@ -44,10 +44,11 @@ function generateConfig(pkg, configs) {
       input,
       output: [
         {
-          file: 'dist/umd/index.js',
-          format: 'umd',
+          file: "dist/umd/index.js",
+          format: "umd",
+          exports: "named",
           // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          name: upperCamel(pkg?.name?.split('/')[1]),
+          name: upperCamel(pkg?.name?.split("/")[1]),
           sourcemap: isDev,
           banner,
         },
@@ -57,9 +58,9 @@ function generateConfig(pkg, configs) {
       input,
       output: [
         {
-          exports: 'auto',
-          file: 'dist/lib/index.js',
-          format: 'cjs',
+          exports: "auto",
+          file: "dist/lib/index.js",
+          format: "cjs",
           sourcemap: isDev,
           banner,
         },
@@ -69,9 +70,9 @@ function generateConfig(pkg, configs) {
       input,
       output: [
         {
-          exports: 'auto',
-          file: 'dist/es/index.mjs',
-          format: 'esm',
+          exports: "auto",
+          file: "dist/es/index.mjs",
+          format: "esm",
           sourcemap: isDev,
           banner,
         },
@@ -82,15 +83,21 @@ function generateConfig(pkg, configs) {
   return [
     ...defaultConfigs.map((entry) => ({
       ...entry,
-      external: ['react/jsx-runtime', ...externals],
+      external: ["react/jsx-runtime", ...externals],
       plugins: [
         eslint({
           throwOnError: true, // lint 结果有错误将会抛出异常
           // throwOnWarning: true,
-          include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.mjs', 'src/**/*.jsx', 'src/**/*.tsx'],
-          exclude: ['node_modules/**', '**/__tests__/**'],
+          include: [
+            "src/**/*.ts",
+            "src/**/*.js",
+            "src/**/*.mjs",
+            "src/**/*.jsx",
+            "src/**/*.tsx",
+          ],
+          exclude: ["node_modules/**", "**/__tests__/**"],
         }),
-        pkg.compiler === 'tsc'
+        pkg.compiler === "tsc"
           ? typescript({
               declaration: false,
             })
@@ -99,23 +106,27 @@ function generateConfig(pkg, configs) {
               swc: {
                 jsc: {
                   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-                  target: ['umd', 'iife'].includes(entry.output[0].format) ? 'es5' : 'es2015',
+                  target: ["umd", "iife"].includes(entry.output[0].format)
+                    ? "es5"
+                    : "es2015",
                 },
               },
-              include: ['./src/**/*.{ts,js,mjs,tsx,jsx}'],
+              include: ["./src/**/*.{ts,js,mjs,tsx,jsx}"],
             }),
         resolve({
           // extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.json'],
         }),
-        commonjs({ extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.json'] }),
+        commonjs({
+          extensions: [".js", ".jsx", ".mjs", ".ts", ".tsx", ".json"],
+        }),
         replace({
           __VERSION__: `${pkg.version}`,
           preventAssignment: true,
         }),
-        isDev && entry.output[0].format === 'umd'
+        isDev && entry.output[0].format === "umd"
           ? serve({
               port: pkg.port || 3000,
-              contentBase: ['public', 'dist'],
+              contentBase: ["public", "dist"],
             })
           : null,
         isDev
@@ -123,26 +134,29 @@ function generateConfig(pkg, configs) {
           : copy({
               copyOnce: true,
               targets: [
-                { src: ['./CHANGELOG.md', './README.md', '../../LICENSE'], dest: './dist' },
                 {
-                  src: './package.json',
-                  dest: './dist',
+                  src: ["./CHANGELOG.md", "./README.md", "../../LICENSE"],
+                  dest: "./dist",
+                },
+                {
+                  src: "./package.json",
+                  dest: "./dist",
                   transform: (contents) => {
                     try {
                       const jsonObj = JSON.parse(contents);
-                      delete jsonObj['scripts'];
-                      delete jsonObj['devDependencies'];
-                      jsonObj['main'] = './lib/index.js';
-                      jsonObj['module'] = './es/index.mjs';
-                      jsonObj['types'] = './types/index.d.ts';
-                      jsonObj['files'] = [
-                        'lib',
-                        'es',
-                        'umd',
-                        'types',
-                        'CHANGELOG.md',
-                        'README.md',
-                        'LICENSE',
+                      delete jsonObj["scripts"];
+                      delete jsonObj["devDependencies"];
+                      jsonObj["main"] = "./lib/index.js";
+                      jsonObj["module"] = "./es/index.mjs";
+                      jsonObj["types"] = "./types/index.d.ts";
+                      jsonObj["files"] = [
+                        "lib",
+                        "es",
+                        "umd",
+                        "types",
+                        "CHANGELOG.md",
+                        "README.md",
+                        "LICENSE",
                       ];
                       contents = JSON.stringify(jsonObj);
                     } catch (error) {}
@@ -156,7 +170,7 @@ function generateConfig(pkg, configs) {
     })),
     {
       input: defaultConfigs[0].input,
-      output: [{ file: 'dist/types/index.d.ts', format: 'es' }],
+      output: [{ file: "dist/types/index.d.ts", format: "es" }],
       plugins: [dts()],
       external: [/\.(css|less|scss|sass)$/],
     },
