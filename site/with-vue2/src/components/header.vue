d0af286570a6db3db3f25e04072081478eff5d17
ShineShao <xiaoshaoqq@gmail.com>
2024-10-02 17:36:22 +0800

    feat: add whit-vue2 site

---
+++ site/with-vue2/src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="_header">
+    <div style="width: 600px; height: 400px">Vue 2.7</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+  mounted: () => {},
+  methods: {},
+};
+</script>
