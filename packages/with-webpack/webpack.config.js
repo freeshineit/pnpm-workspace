2bc5d50e8e1f50e4dac8372d5143aa1caac21d1a
ShineShao <xiaoshaoqq@gmail.com>
2024-10-06 12:36:14 +0800

    feat: support css less sass

--- packages/with-webpack/webpack.config.js
+++ packages/with-webpack/webpack.config.js
@@ -6,14 +6,14 @@ const isDev = environment === "development";
 
 module.exports = {
   mode: environment,
-  entry: "./src/index.tsx",
-  devtool: !isDev ? false : "inline-source-map",
+  entry: "./src/index.ts",
+  devtool: !isDev ? false : "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
     library: isDev
       ? {
-          name: "Foo",
+          name: "Components",
           type: "umd",
         }
       : {
@@ -32,8 +32,80 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+            plugins: [
+              "@babel/plugin-proposal-decorators",
+              { version: "2023-11" },
+            ],
+          },
+        },
+      },
       // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "less-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
     ],
   },
   externals: {
@@ -49,6 +121,6 @@ module.exports = {
     static: {
       directory: path.join(__dirname, "public"),
     },
-    port: 3000,
+    port: 3001,
   },
 };
