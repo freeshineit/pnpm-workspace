6ad917db58ab3372a8996fb8fe8900ea5a6fbddd
ShineShao <xiaoshaoqq@gmail.com>
2026-04-26 01:55:41 +0800

    chore: remove jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  // testEnvironment: "jsdom",
   roots: ["<rootDir>/packages"],
   testMatch: ["**/__tests__/**/?(*.)+(spec|test).ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
