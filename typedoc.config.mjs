745e3635f25047d704cddba193527f4ee1fed0b6
ShineShao <xiaoshaoqq@gmail.com>
2026-04-26 23:30:27 +0800

    chore: remove inclueVersion

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -10,7 +10,7 @@ export default {
   out: "docs",
   plugin: ["typedoc-plugin-rename-defaults", "typedoc-plugin-mdn-links", "typedoc-plugin-replace-text"],
   exclude: ["node_modules", "__tests__/**/*", "packages/*/{__tests__,e2e}/**/*"],
-  includeVersion: true,
+  // includeVersion: true,
   hideGenerator: true,
   disableSources: false,
   tsconfig: "tsconfig.json",
