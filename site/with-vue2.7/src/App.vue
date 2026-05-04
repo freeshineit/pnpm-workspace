8ceba6ec7585d431aec6bc847a7e0838513bafd0
ShineShao <xiaoshaoqq@gmail.com>
2026-05-04 13:19:33 +0800

    feat: update vue2.7

--- site/with-vue2.7/src/App.vue
+++ site/with-vue2.7/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <WebComponent />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import WebComponent from "./components/WebComponent.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    WebComponent,
   },
 };
 </script>
---
+++ site/with-vue2.7/src/components/WebComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <wc-button>Web Component Button</wc-button>
+
+    <wc-picker trigger="click" content='<div style="padding: 12px; background: #fff;">Picker Content</div>' placement="bottom"> <button>打开 Picker</button></wc-picker>
+
+    <wc-popover
+      content="这是一个 Popover 示例!"
+      list='[
+    {
+      "value": 1,
+      "label": "1x"
+    },
+    {
+      "value": 2,
+      "label": "2x"
+    },
+    {
+      "value": 3,
+      "label": "3x"
+    },
+    {
+      "value": 4,
+      "label": "4xxxxxxxxxxxxxxxxx"
+    }
+  ]'
+      open="1">
+      <wc-button>点击我</wc-button>
+    </wc-popover>
+  </div>
+</template>
+
+<script>
+import "@ak2021/web-components/dist/style/css.js";
+import "@ak2021/web-components";
+
+export default {
+  name: "WebComponent",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
