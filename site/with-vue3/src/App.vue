86d0a7d4f52be06757759c28e200bc69f2fdde9b
ShineShao <xiaoshaoqq@gmail.com>
2026-05-04 13:01:37 +0800

    chore: update demo

--- site/with-vue3/src/App.vue
+++ site/with-vue3/src/App.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import WebComponent from "./components/WebComponent.vue";
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    <div class="wrapper">
+      <WebComponent />
+    </div>
   </main>
 </template>
 
---
+++ site/with-vue3/src/components/WebComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import "@ak2021/web-components/dist/style/css.css";
+import "@ak2021/web-components";
+</script>
+
+<template>
+  <div class="greetings">
+    <wc-button>Web Component Button</wc-button>
+
+    <wc-picker trigger="click" content='<div style="padding: 12px; background: #fff;">Picker Content</div>' placement="bottom"> <button>打开 Picker</button></wc-picker>
+
+    <wc-popover
+      content="这是一个 Popover 示例!"
+      list='[
+    {
+      "value": 1,
+      "label": "1x"
+    },
+    {
+      "value": 2,
+      "label": "2x"
+    },
+    {
+      "value": 3,
+      "label": "3x"
+    },
+    {
+      "value": 4,
+      "label": "4xxxxxxxxxxxxxxxxx"
+    }
+  ]'
+      open="1">
+      <wc-button>点击我</wc-button>
+    </wc-popover>
+  </div>
+</template>
